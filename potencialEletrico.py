de7a4a24d3fe74ab11dc95a1616a704f46350db9
Alandersonjs <limaalanderson@gmail.com>
2023-08-04 08:32:01 -0300

    linhas

--- potencialEletrico.py
+++ potencialEletrico.py
@@ -12,3 +12,19 @@ def desenhar_cargas(cargas, posicoes):
     for i, carga in enumerate(cargas):
         plt.scatter(posicoes[i][0], posicoes[i][1], s=100 * abs(carga), c='red' if carga > 0 else 'blue')
         plt.text(posicoes[i][0], posicoes[i][1], f'Carga {i+1}: {carga}', ha='center', va='center')
+#Função para desenhar as linhas equipotenciais
+def desenhar_linha_equipotencial(cargas, posicoes, ponto_selecionado):
+    potencial_selecionado = sum(potencial_electrico(c, p, ponto_selecionado) for c, p in zip(cargas, posicoes))
+    x = np.linspace(-10, 10, 100)
+    y = np.linspace(-10, 10, 100)
+    X, Y = np.meshgrid(x, y)
+    Z = sum(potencial_electrico(c, p, (X, Y)) for c, p in zip(cargas, posicoes))
+    plt.contour(X, Y, Z, levels=[potencial_selecionado], colors='green', linestyles='dashed')
+
+
+def main():
+    cargas = []
+    posicoes = []
+    quantidade_cargas = int(input("Digite a quantidade de cargas elétricas (de 1 a 4): "))
+
+        
