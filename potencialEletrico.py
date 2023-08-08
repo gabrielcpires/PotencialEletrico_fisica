9612bb82a42c194e1175fdc083b480eed268ffd2
gabrielcpires <gabrielpiresc2010@gmail.com>
2023-08-08 20:19:10 -0300

    gabriel

--- potencialEletrico.py
+++ potencialEletrico.py
@@ -1,16 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Função para calcular o potencial elétrico em um ponto devido a uma carga
-
+# Constante eletrostática
+K = 8.99e9
 
 def potencial_electrico(carga, posicao, ponto):
-    k = 8.99e9  # Constante eletrostática
-    distancia = np.sqrt((posicao[0] - ponto[0])*2 + (posicao[1] - ponto[1])*2)
-    return k * carga / distancia
-
-# Função para desenhar as cargas no gráfico
-
+    distancia = np.sqrt((posicao[0] - ponto[0])**2 + (posicao[1] - ponto[1])**2)
+    return K * carga / distancia
 
 def desenhar_cargas(cargas, posicoes):
     for i, carga in enumerate(cargas):
@@ -18,26 +14,23 @@ def desenhar_cargas(cargas, posicoes):
                     abs(carga), c='red' if carga > 0 else 'blue')
         plt.text(posicoes[i][0], posicoes[i][1],
                  f'Carga {i+1}: {carga}', ha='center', va='center')
-# Função para desenhar as linhas equipotenciais
 
+def calcular_potencial_total(cargas, posicoes, ponto):
+    return sum(potencial_electrico(c, p, ponto) for c, p in zip(cargas, posicoes))
 
-def desenhar_linha_equipotencial(cargas, posicoes, ponto_selecionado):
-    potencial_selecionado = sum(potencial_electrico(
-        c, p, ponto_selecionado) for c, p in zip(cargas, posicoes))
-    x = np.linspace(-10, 10, 100)
-    y = np.linspace(-10, 10, 100)
+def criar_linha_equipotencial(cargas, posicoes, potencial, ponto_selecionado):
+    x = np.linspace(-10, 10, 400)
+    y = np.linspace(-10, 10, 400)
     X, Y = np.meshgrid(x, y)
     Z = sum(potencial_electrico(c, p, (X, Y))
             for c, p in zip(cargas, posicoes))
-    plt.contour(X, Y, Z, levels=[potencial_selecionado],
-                colors='green', linestyles='dashed')
-
+    plt.contour(X, Y, Z, levels=[potencial], colors='green', linestyles='dashed')
 
 def main():
     cargas = []
     posicoes = []
     quantidade_cargas = int(input("Digite a quantidade de cargas elétricas (de 1 a 4): "))
-    # Capturar informações sobre cada carga e sua posição
+    
     for i in range(quantidade_cargas):
         carga = float(input(f"Digite o valor da carga {i+1}: "))
         posicao_x = float(input(f"Digite a posição x da carga {i+1}: "))
@@ -45,29 +38,23 @@ def main():
         cargas.append(carga)
         posicoes.append((posicao_x, posicao_y))
 
-    # Configurar o gráfico
     plt.figure(figsize=(8, 8))
     plt.xlabel('Eixo x')
     plt.ylabel('Eixo y')
     desenhar_cargas(cargas, posicoes)
     plt.title('Potencial Elétrico de uma Distribuição de Cargas')
-    # teste
+
     while True:
-        # Capturar o ponto selecionado pelo usuário com o mouse
         ponto_selecionado = plt.ginput(1, show_clicks=True)[0]
         plt.scatter(ponto_selecionado[0], ponto_selecionado[1], s=100, c='black', marker='x')
 
-        # Calcular e exibir o potencial elétrico no ponto selecionado
-        potencial_no_ponto = sum(potencial_electrico(c, p, ponto_selecionado) for c, p in zip(cargas, posicoes))
+        potencial_no_ponto = calcular_potencial_total(cargas, posicoes, ponto_selecionado)
         print(f'Potencial Elétrico no ponto selecionado: {potencial_no_ponto:.2f} V')
 
-        # Desenhar a linha equipotencial que passa pelo ponto selecionado
-        desenhar_linha_equipotencial(cargas, posicoes, ponto_selecionado)
+        criar_linha_equipotencial(cargas, posicoes, potencial_no_ponto, ponto_selecionado)
         plt.draw()
 
     plt.show()
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
-
-
